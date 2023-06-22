5004ecffe2803972032e843375a5ed88b647aec1
bittu1943 <136315448+bittu1943@users.noreply.github.com>
2023-06-22 15:41:42 +0530

    Add files via upload

---
+++ 1000times.js
@@ -0,0 +1,13 @@
+function myFunction(){
+    var x = document.getElementById("your-name").value;
+    alert("happy birthday " + x + ".");
+    alert("happy birthday " + x + ".");
+    alert("happy birthday " + x + ".");
+    alert("happy birthday " + x + ".");
+    alert("happy birthday " + x + ".");
+    alert("happy birthday " + x + ".");
+    alert("happy birthday " + x + ".");
+    alert("happy birthday " + x + ".");
+
+
+} 
