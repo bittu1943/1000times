faed576c5cdbfe37db609eec34526f26c5403b67
bittu1943 <136315448+bittu1943@users.noreply.github.com>
2023-06-22 15:52:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 function myFunction(){
     var x = document.getElementById("your-name").value;
     alert("happy birthday " + x + ".");
-    alert("happy birthday " + x + ".");
-    alert("happy birthday " + x + ".");
-    alert("happy birthday " + x + ".");
-    alert("happy birthday " + x + ".");
-    alert("happy birthday " + x + ".");
-    alert("happy birthday " + x + ".");
-    alert("happy birthday " + x + ".");
+  
 
 
 } 
